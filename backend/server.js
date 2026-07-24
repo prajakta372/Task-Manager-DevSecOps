0fa587557b90cf6dae3e72b5e7dfcf46e5e7e16f
prajakta372 <prajaktagavhane372@gmail.com>
2026-07-24 17:39:48 +0530

    server.js package.json

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const morgan = require('morgan');
 const { Pool } = require('pg');
 
